f2c3e5e7e1bb783db6eb266698afc8c70fec56a6
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-22 21:42:40 +0500

    fix(layout): clean up SocialLinks.tsx and verify icon imports

--- src/components/layout/SocialLinks.tsx
+++ src/components/layout/SocialLinks.tsx
@@ -3,8 +3,8 @@ import { Github, Twitter, MessageCircle } from 'lucide-react';
 
 export const SocialLinks = () => (
   <div className='flex gap-6'>
-    <a href='#' className='text-neutral-500 hover:text-white transition-colors'><Github className='w-5 h-5' /></a>
-    <a href='#' className='text-neutral-500 hover:text-white transition-colors'><Twitter className='w-5 h-5' /></a>
-    <a href='#' className='text-neutral-500 hover:text-white transition-colors'><MessageCircle className='w-5 h-5' /></a>
+    <a href='#' className='text-neutral-500 hover:text-white transition-all duration-300 hover:-translate-y-1'><Github className='w-5 h-5' /></a>
+    <a href='#' className='text-neutral-500 hover:text-white transition-all duration-300 hover:-translate-y-1'><Twitter className='w-5 h-5' /></a>
+    <a href='#' className='text-neutral-500 hover:text-white transition-all duration-300 hover:-translate-y-1'><MessageCircle className='w-5 h-5' /></a>
   </div>
 );
