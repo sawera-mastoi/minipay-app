eb2f70b6bb175b054e8fdd2ff423738565afd992
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-22 21:13:36 +0500

    chore(constants): add explorer URL and supported wallets list

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,4 @@
 export const CONTRACT_ADDRESS = '0xc8b20FD2C23f2Db1d7f27736B0E20bdaFbF2672c';
+
+export const CELO_EXPLORER_URL = 'https://celoscan.io';
+export const SUPPORTED_WALLETS = ['OKX', 'BITGET', 'METAMASK'];
